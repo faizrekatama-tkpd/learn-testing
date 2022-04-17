c320d84086ded44a8453139659a38fcf2d888b80
Faiz Azmi Rekatama <faiz.rekatama@tokopedia.com>
2022-04-17 21:58:06 +0700

    test: add comments

--- src/components/ButtonTitle/__tests__/ButtonTitle.test.tsx
+++ src/components/ButtonTitle/__tests__/ButtonTitle.test.tsx
@@ -6,15 +6,23 @@ import ButtonTitle from '../index';
 test('should show title when click cta', () => {
   render(<ButtonTitle />);
 
+  /**
+   * Lets make sure the title is not shown
+   * on initial rendered
+   */
   const title = screen.queryByText(/the title/i); // we use queryByText to avoid throwing error if using getByText
-
   expect(title).not.toBeInTheDocument(); // test assertion for non existence element in the DOM
 
+  /**
+   * Lets check if the button exist first
+   */
   const btnClickMe = screen.getByTestId('btnClickMe'); // element selector
-
   expect(btnClickMe).toBeInTheDocument(); // test assertion
 
+  /**
+   * Lets click the button
+   * and see if the title shown
+   */
   userEvent.click(btnClickMe) // triggering click event on btnClickMe button
-
   expect(screen.getByText(/the title/i)).toBeInTheDocument(); // element selector with regex pattern + test assertion
 });
